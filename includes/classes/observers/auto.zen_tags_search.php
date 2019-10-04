73a3262da73657812172de09f5eba5cd68d0cf4c
lat9 <lat9@vinosdefrutastropicales.com>
2019-10-04 11:11:55 -0400

    Correct storefront search processing

--- includes/classes/observers/auto.zen_tags_search.php
+++ includes/classes/observers/auto.zen_tags_search.php
@@ -11,7 +11,8 @@ class zcObserverZenTagsSearch extends base
         if (defined('ZEN_TAGS_ENABLE') && ZEN_TAGS_ENABLE == 'true') {
             $this->tID = (isset($_GET['tID']) && ((int)$_GET['tID']) > 0) ? ((int)$_GET['tID']) : false;
             $this->order_by = '';
-            if ($this->tID !== false || (defined('ZEN_TAGS_SEARCH_ALWAYS') && ZEN_TAGS_SEARCH_ALWAYS == 'true')) {
+            $this->search_tags = (defined('ZEN_TAGS_SEARCH_ALWAYS') && ZEN_TAGS_SEARCH_ALWAYS == 'true');
+            if ($this->tID !== false || $this->search_tags) {
                 $this->attach(
                     $this, 
                     array(
@@ -29,30 +30,14 @@ class zcObserverZenTagsSearch extends base
     {
         switch ($eventID) {
             case 'NOTIFY_SEARCH_SELECT_STRING':
-                if (!empty($GLOBALS['keywords']) && !empty($_GET['keyword']) && zen_parse_search_string(stripslashes($_GET['keyword']), $search_keywords)) {
-                    $in_tag_select = '';
-                    foreach ($search_keywords as $current_keyword) {
-                        switch ($current_keyword) {
-                            case '(':
-                            case ')':
-                            case 'and':
-                            case 'or':      //- Fall-through from above ...
-                                $in_tag_select .= " $current_keyword ";
-                                break;
-
-                            default:
-                                $in_tag_select .= "t.tag_name LIKE '%:keywords%'";
-                                $in_tag_select = $GLOBALS['db']->bindVars($in_tag_select, ':keywords', $current_keyword, 'noquotestring');
-                                break;
-                        }
-                    }
-                    $GLOBALS['select_str'] .= ", IF (t2p.tag_mapping_id = p.products_id and t2p.tag_id = t.tag_id AND ($in_tag_select), 1, 0) AS in_tag ";
-                    $this->order_by = ' in_tag DESC,';
-                }
                 break;
                 
+            // -----
+            //
             case 'NOTIFY_SEARCH_FROM_STRING':
-                $GLOBALS['from_str'] = $this->insertAfter($GLOBALS['from_str'], ' c,', ' ' . TABLE_TAGS . ' t, ');
+                if ($this->tID === false) {
+                    $GLOBALS['from_str'] = $this->insertAfter($GLOBALS['from_str'], ' c,', ' ' . TABLE_TAGS . ' t, ');
+                }
                 $GLOBALS['from_str'] .= PHP_EOL . "LEFT JOIN " . TABLE_TAGS_TO_PRODUCTS . ' t2p ON t2p.tag_mapping_id = p.products_id ';
                 break;
                 
@@ -63,8 +48,8 @@ class zcObserverZenTagsSearch extends base
                     $p2c_pos = strpos($where_str, $p2c_str);
                     $GLOBALS['where_str'] = substr($GLOBALS['where_str'], 0, $p2c_pos + strlen($p2c_str)) . " AND t2p.tag_id = {$this->tID} )";
                 } else {
-                    if (!empty($GLOBALS['keywords']) && isset($_GET['keyword']) && zen_parse_search_string(stripslashes($_GET['keyword']), $search_keywords)) {
-                        $tags_where = 'OR ( t.tag_id = t2p.tag_id AND ';
+                    if (!empty($GLOBALS['keywords']) && zen_parse_search_string(stripslashes($_GET['keyword']), $search_keywords)) {
+                        $tags_where = 'OR ( t.tag_id = t2p.tag_id AND t.languages_id = ' . (int)$_SESSION['languages_id'] . ' AND ';
                         foreach ($search_keywords as $current_keyword) {
                             switch ($current_keyword) {
                                 case '(':
@@ -86,8 +71,13 @@ class zcObserverZenTagsSearch extends base
                 break;
                 
             case 'NOTIFY_SEARCH_ORDERBY_STRING':
+                // -----
+                // If this search was initiated by the "Tag Cloud" sidebox, the search is modified to search **only**
+                // for products matching the submitted 'tag-id'.  Modify the 'keywords', displayed on the advanced_search_results
+                // page to indicate that the search was for the tag-name submitted.
+                //
                 if ($this->tID !== false) {
-                    $GLOBALS['keywords'] = "tag ({$GLOBALS['keywords']})";
+                    $GLOBALS['keywords'] = sprintf(ZEN_TAG_SEARCH_IS_TAG, (string)$_GET['keyword']);
                 }
                 $GLOBALS['listing_sql'] = str_ireplace('order by', 'order by' . $this->order_by, $GLOBALS['listing_sql']);
                 break; 
--- includes/languages/english/extra_definitions/zen_tags_extra_definitions.php
+++ includes/languages/english/extra_definitions/zen_tags_extra_definitions.php
@@ -3,4 +3,14 @@
 // Part of the "Zen Tags" plugin by Cindy Merkin (lat9)
 // Copyright (c) 2014-2019 Vinos de Frutas Tropicales
 //
+// -----
+// This language constant is used as the heading for the zen_tag_cloud sidebox.
+//
 define('ZEN_TAG_HEADING_TAG_CLOUD', 'Tag Cloud');
+
+// -----
+// This language constant is used on the advanced_search_results page when the search
+// is initiated by the zen_tag_cloud sidebox, to indicate to the customer that the search
+// is for the tag-name (filled in where the %s lies in the constant.
+//
+define('ZEN_TAG_SEARCH_IS_TAG', 'tag(%s)');
