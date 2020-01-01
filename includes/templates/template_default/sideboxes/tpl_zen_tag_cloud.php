731192b54656d7e78a877a7ee9c43e6086eef21e
lat9 <cindy@vinosdefrutastropicales.com>
2020-01-01 11:50:06 -0500

    #7: Don't display sidebox when no tags are defined

--- includes/templates/template_default/sideboxes/tpl_zen_tag_cloud.php
+++ includes/templates/template_default/sideboxes/tpl_zen_tag_cloud.php
@@ -1,11 +1,14 @@
 <?php
 /// -----
 // Part of the "Zen Tags" plugin by Cindy Merkin (lat9)
-// Copyright (c) 2014-2019 Vinos de Frutas Tropicales
+// Copyright (c) 2014-2020 Vinos de Frutas Tropicales
 //
 $content = '';
 if (defined('ZEN_TAGS_ENABLE') && ZEN_TAGS_ENABLE == 'true') {
     $zen_tag_cloud = new ZenTags();
-    $content = '<div class="sideBoxContent centeredContent">' . $zen_tag_cloud->makeTagCloud() . '</div>';
-    unset($zen_tag_cloud);
+    $zen_tag_cloud_content = $zen_tag_cloud->makeTagCloud();
+    if ($zen_tag_cloud_content != '') {
+        $content = '<div class="sideBoxContent centeredContent">' . $zen_tag_cloud_content . '</div>';
+    }
+    unset($zen_tag_cloud, $zen_tag_cloud_content);
 }
